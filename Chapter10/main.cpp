e87ac676f5598ce0d1469a0bc96c51ca6c8d1ca2
zpc987 <zpc987@sina.com>
2024-06-13 15:15:08 +0800

    add content of the book

--- Chapter10/main.cpp
+++ Chapter10/main.cpp
@@ -6,8 +6,71 @@
 #include "recipe_10_06.h"
 #include "recipe_10_07.h"
 
+#include "iostream"
+using namespace std;
+
+void fun(const char*& n) {
+	++n;
+	++n;
+}
 int main()
 {
+	//int a = 10, b = 20;
+	//auto f1 = [] {return a; }; // error
+	//auto f2 = [&] {return a++; }; // ok
+	//auto f3 = [=] {return a; }; // ok
+	//auto f4 = [=] {return a++; }; // error
+	//auto f5 = [a] {return a + b; }; // error
+	//auto f6 = [a, &b] {return a + (b++); }; // ok
+	//auto f7 = [=, &b] {return a + (b++); }; // ok
+
+	std::string s{"thisdfadjihofdsafd"};
+
+	const char* c = "5824395320";
+	int num = 10;
+	printf("1-%s\n", c);
+	printf("%d\n",num);
+
+	auto f = [=,&num, &c](const char* n) {
+		n = c + 1;
+		++c;
+		num = 5;
+		++n;
+		++n;
+	};
+	auto fr = [&](const char* n) {
+		n = c + 1;
+		++c;
+		num = 5;
+		++n;
+		++n;
+	};
+	auto fm = [=]  (const char* n) mutable {
+		n = c + 1;
+		++c;
+		num = 5;
+		++n;
+		++n;
+	};
+
+	auto f1 = [](int n) {
+		if (n > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+
+	//fun(s.c_str());
+	if (f1(10)) {
+		cout << "It is True" << endl;
+	}
+	fr(c);
+	printf("2-%s\n", c);
+	printf("%d\n", num);
+	fun(c);
+	
    recipe_10_01::execute();
    recipe_10_02::execute();
    recipe_10_03::execute();
--- README.md
+++ README.md
@@ -43,5 +43,7 @@ The following example shows how to generate projects for Visual Studio 2019:
 cmake -G "Visual Studio 16 2019" -DBOOST_PATH=c:/Work/Libraries/boost_1_73_0/ -DGTEST_PATH=c:/Work/Libraries/googletest_1_10/googletest/include/ -DGTEST_PATH_LIB=c:/Work/Libraries/googletest_1_10/lib/ -DCATCH_PATH=c:/Work/Libraries/Catch2/single_include/ -DUSE_RANGES_V3=ON -DRANGES_PATH=c:/Work/Libraries/range-v3-master/include/ -DCPPCORO_PATH=c:/Work/Libraries/cppcoro-master -S .\ -B .\build
 ```
 
+cmake -G "Visual Studio 17 2022" -DBOOST_PATH=C:/work/boost_1_79_0 -DGTEST_PATH=C:/work/partitioning/kahypar/external_tools/googletest -DSKIP_TEST_PROJECTS=true -S .\ -B .\build
+
 ### Suggestions and Feedback
 [Click here](https://docs.google.com/forms/d/e/1FAIpQLSe5qwunkGf6PUvzPirPDtuy1Du5Rlzew23UBp2S-P3wB-GcwQ/viewform) if you have any feedback or suggestions.
